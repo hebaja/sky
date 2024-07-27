c8276a6ba2c6456932619092e9ea803e1dfee4e2
Henrique <hebaja@yahoo.com.br>
2024-07-27 09:08:05 -0300

    handling errors in rush01.c added

--- rush01.c
+++ rush01.c
@@ -8,6 +8,12 @@ void	print_coord(int *coord);
 void	test_table(int rows, int cols, int **table);
 int	is_coord_valid(int *coord);
 
+void	print_error()
+{
+	ft_putstr("Error");
+	ft_putchar('\n');
+}
+
 int	main(int argc, char **argv)
 {
 	int	*coord;
@@ -19,36 +25,40 @@ int	main(int argc, char **argv)
 	i = 0;
 	rows = 4;
 	cols = 4;
-	if(argc != 2)
+
+	if (argc == 2)
 	{
-		ft_putstr("Error");
-		ft_putchar('\n');
+		coord = malloc(sizeof(int) * 16);
+		fetch_coord(argv[1], coord);
+		if (!is_coord_valid(coord))
+		{
+			print_error();
+			return (0);
+		}
+		table = malloc(rows * sizeof(int *));
+		while (i < cols)
+		{
+			table[i] = malloc(cols * sizeof(int));
+			i++;
+		}
+		print_coord(coord);
+	}
+	else
+	{
+		print_error();
 		return (0);
 	}
-	coord = malloc(sizeof(int) * 16);
-	table = malloc(rows * sizeof(int *));
-	while(i < cols)
-	{
-		table[i] = malloc(cols * sizeof(int));
-		i++;
-	}
-
-	fetch_coord(argv[1], coord);
-
-	ft_putnbr(is_coord_valid(coord));
-	ft_putchar('\n');
-	print_coord(coord);
 
 	/*test_table(rows, cols, table);*/
 	
-	free(coord);
+	free (coord);
 	i = 0;
-	while(i < rows)
+	while (i < rows)
 	{
 		free(table[i]);
 		i++;
 	}
-	free(table);
+	free (table);
 
 	return (0);
 }
